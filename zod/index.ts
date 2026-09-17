d8df6498ac160b1f6a739be000574303ef085e24
snyted <joaofarias_16@hotmail.com>
2026-09-17 12:27:35 -0300

    Add order schema validation with pricing calculation (ex 05)

--- zod/index.ts
+++ zod/index.ts
@@ -71,6 +71,34 @@ const user02 = {
     workDays: ["SegunDa", "Terça", "QuaRta", "QUINTA", "SexTA"]
 }
 
-const { success, data } = user02Schema.safeParse(user02)
+// const { success, data } = user02Schema.safeParse(user02)
 
-console.log(data)
+//--- 05 ---
+
+const orderSchema = z.object({
+    size: z.enum(["P", "M", "G"]),
+    flavor: z.string().min(3),
+    extras: z.array(z.string().min(1)).optional(),
+    price: z.number().optional(),
+}).transform(order => {
+    const basePrice = {
+        "P": 20,
+        "M": 30,
+        "G": 40,
+    }[order.size];
+
+    const extrasPrice = (order.extras?.length || 0) * 5;
+
+    return {
+        ...order,
+        price: basePrice + extrasPrice
+    };
+})
+
+const order = {
+    size: "G",
+    flavor: "Margherita",
+}
+
+const { success, data, error } = orderSchema.safeParse(order)
+console.log(success, data, error)
